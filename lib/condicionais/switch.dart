172288bb93a8cdc2f3eda50919255006d1839823
marcoaurelio <marcoaurelio_dev@outlook.com>
2026-02-04 22:21:54 -0300

    Condicionais

---
+++ lib/condicionais/switch.dart
@@ -0,0 +1,27 @@
+void main() {
+  final int nota = 7;
+
+  switch (nota) {
+    case 10:
+      print("Excelente");
+      break;
+
+    case 9:
+      print("Excelente");
+      break;
+
+    case 8:
+      print("Bom");
+      break;
+
+    case 7:
+      print("Bom");
+      break;
+
+    case 6:
+      print("Regular");
+      break;
+    default:
+      print("Reprovado");
+  }
+}

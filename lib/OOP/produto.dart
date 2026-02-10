e453950bb38af4820d110851a53c69ea18aa873a
marcoaurelio <marcoaurelio_dev@outlook.com>
2026-02-10 00:14:16 -0300

    regras para construit o setter

--- lib/OOP/produto.dart
+++ lib/OOP/produto.dart
@@ -1,15 +1,19 @@
 class Produto {
-  final double _preco;
+  double _preco;
 
   Produto(this._preco);
 
   double get precoComDesconto {
     return _preco > 100 ? _preco * 0.9 : _preco;
   }
+
+  set preco(final double preco) {
+    return _preco <= 0 ? print("Preço inválido") : _preco += preco;
+  }
 }
 
 void main() {
   final p1 = Produto(150);
-
+  p1.preco = 20;
   print(p1.precoComDesconto);
 }

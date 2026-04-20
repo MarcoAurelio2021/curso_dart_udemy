c728ba20b74d4901296af546c40ce6823caa1e88
marcoaurelio <marcoaurelio_dev@outlook.com>
2026-04-19 22:28:16 -0300

    Extension com get

--- lib/OOP/extensions.dart
+++ lib/OOP/extensions.dart
@@ -1,11 +1,27 @@
+/*
+  🧠 Pense assim
+
+   Extension = uma “função colada” no tipo
+   NÃO é uma mudança no dado
+ */
+
 extension DobrarNumero on int {
-  int dobrar() {
-    return this * 2;
-  }
+  int dobrar() => this * 2;
+
+  int triplicar() => this * 3;
+  int get triplo => this * 3;
+
+  // 👉 Extension com getters (sem parênteses)
+  bool get ehPositivo => this >= 0;
 }
 
 void main() {
   final num1 = 3;
 
-  num1.dobrar();
+  print(num1.dobrar());
+  print(num1.triplicar());
+
+  print(num1.ehPositivo);
+
+  print(num1.triplo);
 }

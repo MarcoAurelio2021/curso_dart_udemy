3a73cfc79d59de8d9e8492ad2e8f260b1abbc0f7
marcoaurelio <marcoaurelio_dev@outlook.com>
2026-05-03 19:31:45 -0300

    enum com métodos

--- lib/OOP/enum.dart
+++ lib/OOP/enum.dart
@@ -1,13 +1,42 @@
-enum StatusPedido { pendente, processando, enviado, entregue }
+enum StatusPedido {
+  pendente,
+  processando,
+  enviado,
+  cancelado,
+  entregue;
+
+  String descricao() {
+    switch (this) {
+      case StatusPedido.pendente:
+        return "Aguardando pagamento";
+      case StatusPedido.processando:
+        return "Preparando pedido";
+      case StatusPedido.enviado:
+        return "Saiu para entrega";
+      case StatusPedido.entregue:
+        return "Pedido finalizado";
+      case StatusPedido.cancelado:
+        return "O pedido foi cancelado";
+    }
+  }
+}
+
+enum NivelAcesso {
+  admin("Acesso total"),
+  usuario("Acesso limitado"),
+  visitante("Apenas Leitura");
+
+  final String mensagem;
+
+  const NivelAcesso(this.mensagem);
+}
 
 void main() {
-  final StatusPedido pedido = StatusPedido.entregue;
+  final StatusPedido pedido = StatusPedido.cancelado;
 
   print(pedido.name);
 
-  if (pedido == StatusPedido.entregue) {
-    print("Pedido finalizado");
-  } else {
-    print("Pedido ainda não chegou");
-  }
+  final String status = pedido.descricao();
+
+  print(status);
 }
